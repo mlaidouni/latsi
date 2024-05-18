3f1b77692aa00c0643b3ff1b1ba44be68f89b956
laidouni <mohamed.laidouni@etu.u-paris.fr>
2024-05-18 15:22:08 +0200

    main.ml updated: Version 1.0: Aucune extension. Code formatté.
    Régler le caractère d'échappement

--- main.ml
+++ main.ml
@@ -3,8 +3,35 @@ open Ast
 (* On prend le programme à lire en argument. *)
 let lexbuf = Lexing.from_channel (open_in Sys.argv.(1))
 
-let init_var_list = [("A",0);("B",0);("C",0);("D",0);("E",0);("F",0);("G",0);("H",0);("I",0);("J",0);("K",0);("L",0);("M",0);("N",0);("O",0);("P",0);("Q",0);("R",0);("S",0);
-("T",0);("U",0);("V",0);("W",0);("X",0);("Y",0);("Z",0)]
+let init_var_list =
+  [
+    ("A", 0);
+    ("B", 0);
+    ("C", 0);
+    ("D", 0);
+    ("E", 0);
+    ("F", 0);
+    ("G", 0);
+    ("H", 0);
+    ("I", 0);
+    ("J", 0);
+    ("K", 0);
+    ("L", 0);
+    ("M", 0);
+    ("N", 0);
+    ("O", 0);
+    ("P", 0);
+    ("Q", 0);
+    ("R", 0);
+    ("S", 0);
+    ("T", 0);
+    ("U", 0);
+    ("V", 0);
+    ("W", 0);
+    ("X", 0);
+    ("Y", 0);
+    ("Z", 0);
+  ]
 
 (* Fonction pour initialiser l'environnement *)
 let initialize_env programme =
